74898ca220fb2db1750bf30e5fd3a7e36d777b61
matheusazambuja <matheusazambuja35@gmail.com>
2025-03-11 21:41:54 -0300

    Updates

---
+++ assets/entendendo-serveless-Dksth6yR.js
@@ -0,0 +1,9 @@
+import{R as e}from"./index-DtLtY6pX.js";const a={title:"Awesome Title",author:"Antonio Azambuja",date:"2024-12-30",summary:"Vite is an opinionated web dev build tool that serves your code via native ES Module imports. Vite is an opinionated web dev build tool that serves your code via native ES Module imports",tags:["great","awesome","rad"]},l=`<h2>Título 2</h2>
+<h3>Título 3</h3>
+<p>Texto simples
+Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.</p>
+`,d=`## Título 2\r
+### Título 3\r
+\r
+Texto simples\r
+Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.`,r=function(t){return Object.keys(t).forEach(function(o){t[o]}),e.createElement("div",null,e.createElement("h2",null,"Título 2"),e.createElement("h3",null,"Título 3"),e.createElement("p",null,"Texto simples Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production."))};export{r as ReactComponent,a as attributes,l as html,d as markdown};
