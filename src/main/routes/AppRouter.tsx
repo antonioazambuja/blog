10abecd61447875cf2c3e898811dca249bb6e97f
matheusazambuja <matheusazambuja35@gmail.com>
2025-04-27 17:03:25 -0300

    fix: adjust redirect

--- src/main/routes/AppRouter.tsx
+++ src/main/routes/AppRouter.tsx
@@ -1,29 +1,47 @@
-import { lazy, Suspense } from 'react';
-import { BrowserRouter, Navigate, Route, Routes, useRouteError } from "react-router-dom";
-import Post from '../../presentation/pages/post/Post';
-import Home from '../../presentation/pages/home/Home';
+import { lazy, Suspense } from "react";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useRouteError,
+} from "react-router-dom";
+import Post from "../../presentation/pages/post/Post";
+import Home from "../../presentation/pages/home/Home";
 
-const REPO_BASENAME = '/blog';
-
-const AppLayoutFactoryLazy = lazy(() => import('../factories/layouts/app-layout/app-layout.factory'));
+const AppLayoutFactoryLazy = lazy(
+  () => import("../factories/layouts/app-layout/app-layout.factory")
+);
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter basename={REPO_BASENAME}>
+    <BrowserRouter>
       <Suspense>
         <Routes>
           <Route element={<AppLayoutFactoryLazy />}>
-            <Route index path='/' element={<Navigate to='/blog' />} ErrorBoundary={ErrorBoundary} />
-            <Route path='/blog' element={<Home />} ErrorBoundary={ErrorBoundary}>
-            </Route>
-            <Route path='/blog/:slug' element={<Post />} ErrorBoundary={ErrorBoundary} />
+            <Route
+              index
+              path="/"
+              element={<Navigate to="/blog" />}
+              ErrorBoundary={ErrorBoundary}
+            />
+            <Route
+              path="/blog"
+              element={<Home />}
+              ErrorBoundary={ErrorBoundary}
+            ></Route>
+            <Route
+              path="/blog/:slug"
+              element={<Post />}
+              ErrorBoundary={ErrorBoundary}
+            />
             {/* <Route path='*' element={<Navigate to='/blog' />} ErrorBoundary={ErrorBoundary} /> */}
           </Route>
         </Routes>
       </Suspense>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 function ErrorBoundary() {
   const error = useRouteError();
