cb7465903a271a6b0a0607205562740562310bb5
matheusazambuja <matheusazambuja35@gmail.com>
2025-04-27 17:22:31 -0300

    Updates

--- assets/example-1-YH-GBZCQ.js
+++ assets/example-1-YH-GBZCQ.js
@@ -1,4 +1,4 @@
-import{R as e}from"./index-DtLtY6pX.js";const l={title:"Awesome Title",author:"Antonio Azambuja",date:"2024-12-30",tags:["great","awesome","rad"]},a=`<h1>This is awesome</h1>
+import{R as e}from"./index-BDshSQtm.js";const l={title:"Awesome Title",author:"Antonio Azambuja",date:"2024-12-30",tags:["great","awesome","rad"]},a=`<h1>This is awesome</h1>
 <h1>Título 1</h1>
 <h2>Título 2</h2>
 <h3>Título 3</h3>
