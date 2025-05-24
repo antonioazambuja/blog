7037011cff4eb5d4562dd7c46e61a74f748c2409
Antonio Azambuja <antonio.azambuja@ifood.com.br>
2025-05-24 10:18:59 -0300

    Updates

---
+++ assets/example-4-U_30-f3K.js
@@ -0,0 +1,14 @@
+import{R as e}from"./index-BdFgWOgF.js";const l={title:"Awesome Title",author:"Antonio Azambuja",date:"2024-12-30",tags:["great","awesome","rad"]},a=`<h1>This is awesome</h1>
+<h1>Título 1</h1>
+<h2>Título 2</h2>
+<h3>Título 3</h3>
+<p>Texto simples
+Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.</p>
+`,u=`# This is awesome
+
+# Título 1
+## Título 2
+### Título 3
+
+Texto simples
+Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.`,s=function(t){return Object.keys(t).forEach(function(o){t[o]}),e.createElement("div",null,e.createElement("h1",null,"This is awesome"),e.createElement("h1",null,"Título 1"),e.createElement("h2",null,"Título 2"),e.createElement("h3",null,"Título 3"),e.createElement("p",null,"Texto simples Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production."))};export{s as ReactComponent,l as attributes,a as html,u as markdown};
---
+++ assets/example-5-DNkJYFjv.js
@@ -0,0 +1,28 @@
+import{R as e}from"./index-BdFgWOgF.js";const a={title:"Artigo 5",author:"Antonio Azambuja",date:"2024-12-30",tags:["great","awesome","rad"]},i=`<h1>This is awesome</h1>
+<h1>Título 1</h1>
+<h2>Título 2</h2>
+<h3>Título 3</h3>
+<p>Texto simples
+Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.</p>
+<pre><code class="language-js">// Comentário
+const text = 'Texto'
+console.log(text)
+</code></pre>
+`,s=`# This is awesome
+
+# Título 1
+## Título 2
+### Título 3
+
+Texto simples
+Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.
+
+\`\`\`js
+// Comentário
+const text = 'Texto'
+console.log(text)
+\`\`\`
+`,r=function(t){return Object.keys(t).forEach(function(o){t[o]}),e.createElement("div",null,e.createElement("h1",null,"This is awesome"),e.createElement("h1",null,"Título 1"),e.createElement("h2",null,"Título 2"),e.createElement("h3",null,"Título 3"),e.createElement("p",null,"Texto simples Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production."),e.createElement("pre",null,e.createElement("code",{className:"language-js",dangerouslySetInnerHTML:{__html:`// Coment&amp;#xE1;rio
+const text = &amp;apos;Texto&amp;apos;
+console.log(text)
+`}})))};export{r as ReactComponent,a as attributes,i as html,s as markdown};
