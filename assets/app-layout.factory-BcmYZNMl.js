4cb36dc91d957508f2b8cb6cb9c4fb1502d8ebf4
Antonio Azambuja <antonio.azambuja@ifood.com.br>
2025-06-14 15:38:15 -0300

    Updates

---
+++ assets/app-layout.factory-BcmYZNMl.js
@@ -0,0 +1,11 @@
+import{j as e,F as n,u as r,H as o,a as i,b as d,c,d as x,P as p,O as l}from"./index-CEJIlhNg.js";const h=({...s})=>e.jsx(n,{...s,children:e.jsx("p",{children:"💡 Este blog compartilha insights, práticas e novidades sobre SRE, DevOps e Software Engineer."})}),j=({...s})=>{const a=r(),t=()=>{a("/")};return e.jsxs(o,{...s,children:[e.jsx(i,{onClick:t,children:"Antonio Azambuja"}),e.jsx(d,{children:e.jsx(c,{})})]})},m="96px",u="96px",g=x.section`
+  max-width: 1320px;
+  min-height: calc(100dvh - ${m} - ${u});
+  margin: 0 auto;
+
+  padding: 1rem;
+
+  @media screen and (min-width: 768px) {
+    padding: 1rem 2rem;
+  }
+`,H=()=>e.jsxs(e.Fragment,{children:[e.jsx(j,{}),e.jsx(p,{children:e.jsx(g,{children:e.jsx(l,{})})}),e.jsx(h,{})]}),v=()=>e.jsx(H,{});export{v as default};
--- assets/entendendo-serveless-BvueHK2q.js
+++
@@ -1,9 +0,0 @@
-import{R as e}from"./index-B74SiIcI.js";const a={title:"Awesome Title",author:"Antonio Azambuja",date:"2024-12-30",summary:"Vite is an opinionated web dev build tool that serves your code via native ES Module imports. Vite is an opinionated web dev build tool that serves your code via native ES Module imports",tags:["great","awesome","rad"]},l=`<h2>Título 2</h2>
-<h3>Título 3</h3>
-<p>Texto simples
-Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.</p>
-`,d=`## Título 2
-### Título 3
-
-Texto simples
-Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.`,u=function(t){return Object.keys(t).forEach(function(o){t[o]}),e.createElement("div",null,e.createElement("h2",null,"Título 2"),e.createElement("h3",null,"Título 3"),e.createElement("p",null,"Texto simples Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production."))};export{u as ReactComponent,a as attributes,l as html,d as markdown};
